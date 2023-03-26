291a0017387a138bd15f71f63518786dbb0efb14
arjunsharma149 <72021582+arjunsharma149@users.noreply.github.com>
2023-03-26 17:43:30 +0530

    Card Number Validation

--- src/screens/checkoutPage/component/PaymentInfoPage.js
+++ src/screens/checkoutPage/component/PaymentInfoPage.js
@@ -97,7 +97,7 @@ const PaymentInfoPage = () => {
                         maxLength={16}
                         value={paymentInfo.cardNumber}
                         onChange={handleInputChange}
-                        pattern="^4[0-9]{12}(?:[0-9]{3})?$|^5[1-5][0-9]{14}$|^3[47][0-9]{13}$"
+                        pattern="^(?:4[0-9]{15}|5[1-5][0-9]{14}|3[47][0-9]{13}|[0-9]{16})$"
 
                     />
 
